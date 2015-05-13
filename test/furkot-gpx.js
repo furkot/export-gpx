9af152cfcec8823f4cff09c7dcbc216a96129e60
Damian Krzeminski <pirxpilot@code42day.com>
2015-05-13 07:23:56 -0400

    implementation and tests

--- test/furkot-gpx.js
+++ test/furkot-gpx.js
@@ -1,9 +1,50 @@
 var should = require('should');
-var furkotGpx = require('../');
+var fs = require('fs');
+var path = require('path');
+
+process.env.FURKOT_HOST = 'https://trips.furkot.com';
+var gpx = require('../');
+
+function readFileSync(name) {
+  return fs.readFileSync(path.join(__dirname, name), 'utf8');
+}
 
 describe('furkot-gpx node module', function () {
-  it('must have at least one test', function () {
-    furkotGpx();
-    should.fail('Need to write tests.');
+
+  it('simple trip', function() {
+    var t = require('./fixtures/simple-trip.json'),
+      expected = readFileSync('./fixtures/simple.gpx'),
+      generated = gpx(t);
+    should.exist(generated);
+    generated.should.eql(expected);
+  });
+
+  it('multi trip', function() {
+    var t = require('./fixtures/multi-trip.json'),
+      expected = readFileSync('./fixtures/multi.gpx'),
+      generated = gpx(t);
+    should.exist(generated);
+    generated.should.eql(expected);
+  });
+
+  it('empty polyline', function () {
+    var t = require('./fixtures/empty-polyline.json');
+    should.exist(gpx(t));
+  });
+
+  it('overview routes', function() {
+    var t = require('./fixtures/overview-routes.json'),
+      expected = readFileSync('./fixtures/points.gpx'),
+      generated = gpx(t);
+    should.exist(generated);
+    generated.should.eql(expected);
+  });
+
+  it('pass-thru/skip trip', function() {
+    var t = require('./fixtures/pass-thru-skip-multi-night-trip.json'),
+      expected = readFileSync('./fixtures/pass-thru-skip-multi-night.gpx'),
+      generated = gpx(t);
+    should.exist(generated);
+    generated.should.eql(expected);
   });
 });
